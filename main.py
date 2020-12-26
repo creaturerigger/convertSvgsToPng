989315c7109dd29c6a5540550a8152a1e8c1c96d
VOLKAN BAKIR <volkan.bakir@bahcesehir.edu.tr>
2020-12-26 08:51:33 +0000

    new files generated

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 
 SOURCE_COLOR = 'svg xmlns'
 TARGET_COLOR = 'svg fill="#FFFFFF" xmlns'
-for file in os.listdir("svgs"):
-  tree = etree.parse(open("svgs/" + file, 'r'))
+for file in os.listdir("flat_colored_svgs"):
+  tree = etree.parse(open("flat_colored_svgs/" + file, 'r'))
 
   for element in tree.iter():
     if element.tag.split('}')[1] == 'path':
@@ -16,7 +16,7 @@ for file in os.listdir("svgs"):
   
   newBytes = etree.tostring(tree)
   try:
-    cairosvg.svg2png(bytestring=newBytes, write_to=f"pngs/{file}.png")
+    cairosvg.svg2png(bytestring=newBytes, write_to=f"flat_cl_white_svgs/{file}.png")
   except Exception as e:
     print(e)
     pass
